7b149f886e26cb37de76cc3cc930ae4bea89a9b6
manojgupta86 <55997610+manojgupta86@users.noreply.github.com>
2019-10-04 01:38:24 +0530

    Update proviser.tf

--- proviser.tf
+++ proviser.tf
@@ -1,7 +1,7 @@
 # Define AWS as our provider
 provider "aws" {
-access_key ="AKIAZVXHZPNF5KRNYJ3D"
-secret_key ="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"
+access_key ="AKIAZVXHZPNF5TAOM4W2"
+secret_key ="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"
 region    = "ap-south-1"
 }
 
